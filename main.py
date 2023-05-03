ef1882f625d622959c96a1b9243e8d0376b3d5ed
anshulatul <anshulkushal51@gmail.com>
2023-05-03 16:18:11 +0530

    if __name__ == "__main__":     # serve(app, host="0.0.0.0", port=8080)     app.run(debug=True, port=8150)     # app.run(host="0.0.0.0")

--- main.py
+++ main.py
@@ -1936,6 +1936,6 @@ def update_giftcycle():
 
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8080)
+    #serve(app, host="0.0.0.0", port=8080)
     app.run(debug=True, port=8150)
-    app.run(host="0.0.0.0")
+    #app.run(host="0.0.0.0")
